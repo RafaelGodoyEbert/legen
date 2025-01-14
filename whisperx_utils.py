34768ea50831f2bf2cc14c4c65fb4ab622325d4a
Rafael Godoy <78083427+RafaelGodoyEbert@users.noreply.github.com>
2025-01-14 16:34:13 -0300

    Update whisperx_utils.py

--- whisperx_utils.py
+++ whisperx_utils.py
@@ -39,17 +39,17 @@ def transcribe_audio(model: whisperx.asr.WhisperModel, audio_path: Path, srt_pat
 
     # Transcribe
     with time_task("Running WhisperX transcription engine...", end='\n'):
-        transcribe = model.transcribe(audio=audio, language=lang, batch_size=batch_size, on_progress=progress_callback)
+        transcribe = model.transcribe(audio=audio, language=lang, batch_size=batch_size)
 
     # Align if possible
     if lang in whisperx.alignment.DEFAULT_ALIGN_MODELS_HF or lang in whisperx.alignment.DEFAULT_ALIGN_MODELS_TORCH:
         with time_task(message_start="Running alignment...", end='\n'):
             try:
                 model_a, metadata = whisperx.load_align_model(language_code=lang, device="cuda")
-                transcribe = whisperx.align(transcript=transcribe["segments"], model=model_a, align_model_metadata=metadata, audio=audio, device="cuda", return_char_alignments=True, on_progress=progress_callback)
+                transcribe = whisperx.align(transcript=transcribe["segments"], model=model_a, align_model_metadata=metadata, audio=audio, device="cuda", return_char_alignments=True)
             except Exception:
                 model_a, metadata = whisperx.load_align_model(language_code=lang, device="cpu")  # force load on cpu due errors on gpu
-                transcribe = whisperx.align(transcript=transcribe["segments"], model=model_a, align_model_metadata=metadata, audio=audio, device="cpu", return_char_alignments=True, on_progress=progress_callback)
+                transcribe = whisperx.align(transcript=transcribe["segments"], model=model_a, align_model_metadata=metadata, audio=audio, device="cpu", return_char_alignments=True)
     else:
         print(f"Language {lang} not suported for alignment. Skipping this step")
 
